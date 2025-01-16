2724484f7390b741f7c890cbec3d2037f54f5e50
张嘉凯 <killicestone@126.com>
2025-01-16 15:03:56 +0800

    refactor(utils): 重构工厂函数并添加查询参数校验
    - 简化了 createVerificationMiddleware 函数，仅保留对 POST 请求体的校验
    - 新增 verificationQuery 函数用于校验查询参数
    - 更新了 package.json 和 tsconfig.json，添加了 @koa/router 依赖

--- src/utils/factory.ts
+++ src/utils/factory.ts
@@ -7,15 +7,11 @@ seq
 
 /*
 * 工厂函数接收一个包含 swagger 文档的类或对象
-* 用来校验post传参是否完整和query
+* 用来校验post传参是否完整
 */
-// @ts-ignore
 const createVerificationMiddleware = (swaggerClass: any) => {
   return async (ctx, next) => {
-    // 合并请求体和查询参数
-    const params = {
-      ...ctx.request.body,
-    }
+    const params = ctx.request.body
     // 初始化错误消息数组
     const missingFields: string[] = []
     // 遍历 swagger 文档中的每个属性
@@ -79,52 +75,22 @@ const paginationMiddleware = async (ctx: any, model: any, msg?: string) => {
 }
 
 /*
-* 通过class校验数据
+* 通过class校验query参数是否完整
 * */
-const verificationByClass = async (ctx: any, swaggerClass: any) => {
-  return new Promise((resolve, reject) => {
-      // 合并请求体和查询参数
-      const params = {
-        ...ctx.request?.body,
-        ...ctx.request?.query,
-      }
-      // 初始化错误消息数组
-      const missingFields: string[] = []
-      // 遍历 swagger 文档中的每个属性
-      for (let key in swaggerClass.swaggerDocument) {
-        const paramValue = params[key]
-        const spec = swaggerClass.swaggerDocument[key]
-        if (spec.required) {
-          // 检查是否提供了该字段，并且不是空字符串或仅包含空白字符
-          if (paramValue === undefined || String(paramValue).trim() === '') {
-            // 如果是必填项且未提供有效值，则记录下来
-            missingFields.push(spec.description)
-          }
-        }
-      }
-      // 检查是否有任何必填项缺失或无效
-      if (missingFields.length > 0) {
-        // 写入到error 的log中
-        error(`url:${ctx.request.url},error:${missingFields.join(', ')},${JSON.stringify(ctx.request)}`)
-        // 构建错误响应
-        ctx.body = ctxBodySpecification({
-          code: 400,
-          msg: `以下字段为必填项且不能为空：${missingFields.join(', ')}`,
-        })
-        // 返回不再继续执行后续中间件
-        reject()
-      }
-      resolve(params)
-  })
-
-
+const verificationQuery = async (swaggerClass: any) => {
+  return async (ctx, next) => {
+    const queryParams = Object.assign({ size: 10, current: 1, page: 1 }, ctx.request.query)
+    console.log(swaggerClass.swaggerDocument)
+    console.log(queryParams)
+    await next()
+  }
 }
 
 
 export {
   createVerificationMiddleware,
   paginationMiddleware,
-  verificationByClass
+  verificationQuery
 }
 
 
